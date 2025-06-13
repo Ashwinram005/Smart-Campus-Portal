2d0ef2bdd846525e6ebcd7febc3932e2809ed90c
Ashwinram005 <asheinrammohan2005@gmail.com>
2025-06-13 20:59:18 +0530

    Set up backend

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const connectDB = require('./config/db');
+
+// Load environment variables
+dotenv.config();
+
+// Connect to MongoDB
+connectDB();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Basic route to test
+app.get('/', (req, res) => {
+  res.send('SmartCampusPortal Backend is Running!');
+});
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
