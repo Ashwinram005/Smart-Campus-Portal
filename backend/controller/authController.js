e97ead5e76a2982cb57a79a795768a7e7b532f21
Ashwinram005 <ashwinrammohan2005@gmail.com>
2025-06-14 11:23:06 +0530

    Completed reister and login api

---
+++ backend/controller/authController.js
@@ -0,0 +1,51 @@
+const User = require("../model/User");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+
+const generateToken = (user) => {
+  return jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
+
+exports.register = async (req, res) => {
+  const { name, email, password, role, department, year } = req.body;
+
+  try {
+    const userExists = await User.findOne({ email });
+    if (userExists)
+      return res.status(400).json({ message: "User already exists" });
+
+    const newUser = await User.create({
+      name,
+      email,
+      password,
+      role,
+      department,
+      year,
+    });
+    res.status(201).json({ user: newUser });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "Registration failed", error: err.message });
+  }
+};
+
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ message: "User not found" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch)
+      return res.status(400).json({ message: "Invalid credentials" });
+
+    const token = generateToken(user);
+    res.status(200).json({ user, token });
+  } catch (err) {
+    res.status(500).json({ message: "Login failed", error: err.message });
+  }
+};
