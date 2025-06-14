47d43ce57a2adfe646b0ab181991ef7bb0c29495
SANTHOSH-STARLABS <149648197+SANDY655@users.noreply.github.com>
2025-06-14 13:44:28 +0530

    Included Login,Admin Dashboard and User Management Page with dummy data

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,22 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const connectDB = require('./config/db');
-const authRoutes = require('./routes/authRoutes');
-const cors = require('cors');
-
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+const cors = require("cors");
 
 dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors({
-  origin: 'http://localhost:5173',  // frontend URL
-  credentials: true                 // allow cookies/headers if needed
-}));app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173", // frontend URL
+    credentials: true, // allow cookies/headers if needed
+  })
+);
+app.use(express.json());
 
-app.use('/api/auth', authRoutes);
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
